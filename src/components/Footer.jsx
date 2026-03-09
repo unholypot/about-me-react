2003cd45115b4eda311baadc631f12ec9de57818
Allen Saji <allensaji012@gmail.com>
2026-03-10 02:04:13 +1000

    tweaked footer and removed links...redundant

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,7 @@ function Footer() {
       whileInView="visible"
       viewport={defaultViewport}
     >
-      <nav>
-        <div className="nav-links-container">
-          <ul className="nav-links">
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#experience">Skills &amp; Tools</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+
       <p>Copyright &#169; 2025 Allen Saji.</p>
       <p> Built with React {"//"} Running on Vercel.</p>
     </motion.footer>
